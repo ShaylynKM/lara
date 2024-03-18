c608deb112345dfbdd4b3125795e5800692a4d5c
ShaylynKM <shaylynmelin@gmail.com>
2024-03-18 00:14:49 -0600

    Started Scale
    Can interpolate between medium and small + medium and large values. Currently continues scaling over and over for as long as you drag. Need to find a way to stop scaling once reaching the desired value + keep track of mouse position in relation to the object rather than globally

--- Assets/Scripts/Player/Transformation/Scale.cs
+++ Assets/Scripts/Player/Transformation/Scale.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scale : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 3 different sizes the object can be
+    [SerializeField] private float smallValue;
+    [SerializeField] private float mediumValue;
+    [SerializeField] private float largeValue;
+
+    private Vector3 smallScale;
+    private Vector3 mediumScale;
+    private Vector3 largeScale;
+
+    private int frameCount = 60; // The amount of frames to interpolate between the different sizes
+    private int framesElapsed = 0;
+
+    private void Awake()
+    {
+        smallValue = .5f;
+        mediumValue = 1.5f;
+        largeValue = 3f;
+
+        // Initializing the sizes for the object
+        smallScale = new Vector3(smallValue, smallValue, smallValue);
+        mediumScale = new Vector3(mediumValue, mediumValue, mediumValue);
+        largeScale = new Vector3(largeValue, largeValue, largeValue);
+
+        transform.localScale = mediumScale; // Sets the object to its medium size before the game starts
+    }
+
+    private void OnMouseDown()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
+    {
+        int addFrame = 1;
+
+        framesElapsed = (framesElapsed + addFrame) % (frameCount + addFrame); // Resets framesElapsed to 0 after reaching the desired value
+
+        float interpolationRatio = (float)framesElapsed / frameCount; // Casts these variables as floats before dividing to get our ratio
+
+        // Mouse dragging up
+        if(Input.GetAxis("Mouse Y") < 0)
+        {
+            Debug.Log("Smaller???");
+            transform.localScale = Vector3.Lerp(mediumScale, smallScale, interpolationRatio);
+        }
+
+        // Mouse dragging down
+        if (Input.GetAxis("Mouse Y") > 0)
+        {
+            Debug.Log("Bigger?!?!??!");
+            transform.localScale = Vector3.Lerp(mediumScale, largeScale, interpolationRatio);
+        }
+    }
+
+    private void OnMouseUp()
     {
         
     }
+
 }
