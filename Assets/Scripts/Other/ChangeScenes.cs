c83f4eabb4a2699188d129ac1f7aa1373d9a1b0d
ShaylynKM <shaylynmelin@gmail.com>
2024-03-20 19:00:16 -0600

    Added one variable + comment

--- Assets/Scripts/Other/ChangeScenes.cs
+++ Assets/Scripts/Other/ChangeScenes.cs
@@ -6,11 +6,14 @@ using UnityEngine.SceneManagement;
 public class ChangeScenes : MonoBehaviour
 {
     // This script requires all the levels to be in the correct order in the build settings.
+    // NOTE: This isn't the best way to do this if we have scenes we want to go back and forth between.
 
     public void LoadNextScene()
     {
+        int nextScene = 1;
+
         // Loads the next scene in the build index
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + nextScene);
     }
 
 }
