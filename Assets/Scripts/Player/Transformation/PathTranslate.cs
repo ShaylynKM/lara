01c0fc35f25b1ec295dbf2eff01086fa19d01ce0
anna <annavgk@gmail.com>
2024-04-02 15:17:56 -0600

    Sean Fixes

--- Assets/Scripts/Player/Transformation/PathTranslate.cs
+++ Assets/Scripts/Player/Transformation/PathTranslate.cs
@@ -29,16 +29,41 @@ public class PathTranslate : MonoBehaviour
                 isDragging = true;
             }
         }
+        if(Input.GetMouseButton(0) && isDragging)
+        {
+            // Check if the mouse release position is close enough to the target path point
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 projectedPoint = Vector3.Project((mousePos - pathPoints[0].position), (pathPoints[1].position - pathPoints[0].position)) + pathPoints[0].position;
+            float pathDistance = (pathPoints[0].position - pathPoints[1].position).magnitude;
+            float p0Distance = (pathPoints[0].position - projectedPoint).magnitude;
+            float p1Distance = (pathPoints[1].position - projectedPoint).magnitude;
+            if(p0Distance > pathDistance || p1Distance > pathDistance)
+            {
+                projectedPoint = GetClosestPathPoint().position;
+            }
+
+            mousePos.z = transform.position.z;
+            transform.position = projectedPoint;
+        }
 
         if (Input.GetMouseButtonUp(0) && isDragging)
         {
             // Check if the mouse release position is close enough to the target path point
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = transform.position.z; // Ensure z-position is the same as the object
-            float distanceToTarget = Vector3.Distance(mousePos, closestPoint.position);
+            Vector3 projectedPoint = Vector3.Project((mousePos - pathPoints[0].position), (pathPoints[1].position - pathPoints[0].position)) + pathPoints[0].position;
+            float pathDistance = (pathPoints[0].position - pathPoints[1].position).magnitude;
+            float p0Distance = (pathPoints[0].position - projectedPoint).magnitude;
+            float p1Distance = (pathPoints[1].position - projectedPoint).magnitude;
+            if (p0Distance > pathDistance || p1Distance > pathDistance)
+            {
+                projectedPoint = GetClosestPathPoint().position;
+            }
+            Vector3 closestPoint =GetClosestPathPoint().position;
+            // Ensure z-position is the same as the object
+            float distanceToTarget = Vector3.Distance(projectedPoint, closestPoint);
             if (distanceToTarget < snapDistance)
             {
-                transform.position = closestPoint.position;
+                transform.position = closestPoint;
                 Debug.Log("Puzzle Solved!");
             }
             isDragging = false;
