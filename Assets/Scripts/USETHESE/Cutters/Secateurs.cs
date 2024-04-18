a3bcd579a7ffb1d2a050552e11182306a7a04a40
ShaylynKM <shaylynmelin@gmail.com>
2024-04-17 18:13:34 -0600

    Scissors animated snip

--- Assets/Scripts/USETHESE/Cutters/Secateurs.cs
+++ Assets/Scripts/USETHESE/Cutters/Secateurs.cs
@@ -8,6 +8,21 @@ public class Secateurs : MonoBehaviour
 {
     private float zValue = 0f;
 
+    [SerializeField]
+    private Sprite open;
+
+    [SerializeField]
+    private Sprite closed;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+;       spriteRenderer.sprite = open;
+    }
+
     private void Update()
     {
         // Makes the secateurs follow the mouse. Cannot put it down like in the freetranslate script.
@@ -16,6 +31,26 @@ public class Secateurs : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
         mousePos.z = zValue;
         transform.position = mousePos;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            OnMouseDown();
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            OnMouseUp();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        spriteRenderer.sprite = closed;
+    }
+
+    private void OnMouseUp()
+    {
+        spriteRenderer.sprite = open;
     }
 
 }
